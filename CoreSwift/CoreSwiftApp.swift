1acbe48142c7f586f82978c513b1370157bc67e5
Ariful0007 <arifulpub143@gmail.com>
2023-05-16 14:27:11 +0600

    Initial Commit

---
+++ CoreSwift/CoreSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreSwiftApp.swift
+//  CoreSwift
+//
+//  Created by sang on 16/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
